2ff850d9ff9d3d54ea5c2b570f1dd54565bb440b
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-09-11 21:06:53 +0300

    upd

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,16 +2,7 @@ export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      origin: [
-        "https://project-vercel-o76e-6ae2yim05-vasyls-projects-374bff85.vercel.app", // твій фронт
-        "http://localhost:3000", // для локальної розробки
-      ],
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
