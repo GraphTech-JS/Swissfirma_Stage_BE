b9b1d1947461e138b1949c14cfbe9052d9773915
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-09-12 19:06:27 +0300

    upd

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,19 +1,8 @@
-const allowedOrigins = [
-  "http://localhost:3000", // локальний фронтенд
-  "https://project-vercel-o76e.vercel.app", // продакшн фронтенд
-];
-
 export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: allowedOrigins,
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
